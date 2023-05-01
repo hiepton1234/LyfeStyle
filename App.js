3addbabe8d1581c5c0b9e64c54dd2a4eb8b16822
Seth Tran <ssethtran7@gmail.com>
2023-05-01 12:13:23 -0700

    bar graph for sleep

--- App.js
+++ App.js
@@ -1,41 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Platform } from 'react-native';
-import {useState} from "react";
-import DateTimePicker from '@react-native-community/datetimepicker';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { BarChart } from 'react-native-chart-kit';
 
 
   export default function App() {
-    const [date, setDate] = useState(new Date());
-    const [showPicker, setShowPicker] = useState(true);
-
-    const handleDateChange = (event, selectedDate) => {
-      setShowPicker(Platform.OS === 'ios');
-      setDate(selectedDate || date);
+    const data = {
+      labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      datasets: [{
+          data: [7.5, 8, 7, 6, 6.5, 9, 8.5],
+        }],
     };
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Personicle</Text>
       <Text style={styles.subtitle}>Sleep</Text>
-      {showPicker && (
-          <DateTimePicker
-              value={date}
-              mode="time"
-              is24Hour={true}
-              display="default"
-              onChange={handleDateChange}
-          />
-      )}
-      <Text style={styles.subtitle}> to </Text>
-      {showPicker && (
-          <DateTimePicker
-              value={date}
-              mode="time"
-              is24Hour={true}
-              display="default"
-              onChange={handleDateChange}
-          />
-      )}
+        <View style={{ alignItems: 'center' }}>
+            <ScrollView horizontal={true}>
+                <BarChart
+                    data={data}
+                    width={700} // adjust the width of the chart to fit the screen
+                    height={250}
+                    chartConfig={{
+                        backgroundGradientFrom: '#f0f0f0',
+                        backgroundGradientTo: '#e0e0e0',
+                        decimalPlaces: 0,
+                        color: (opacity = 1) => `rgba(0, 0, 255, ${opacity})`,
+                        style: {
+                            borderRadius: 16,
+                        },
+                    }}
+                    style={{
+                        marginVertical: 8,
+                        borderRadius: 16,
+                    }}
+                    withInnerLines={false}
+                    bezier
+                />
+            </ScrollView>
+        </View>
       <StatusBar style="auto" />
     </View>
   );
