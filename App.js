bd59d1ec03f9a44575a8d525bf0690835c8b5795
Seth Tran <ssethtran7@gmail.com>
2023-05-01 08:10:21 -0700

    title and sleep subtitle plus text input

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import {useState} from "react";
 
-export default function App() {
+
+  export default function App() {
+    const [text, setText] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Personicle</Text>
+      <Text style={styles.subtitle}>Sleep</Text>
+      <TextInput
+          style={styles.input}
+          onChangeText={text => setText(text)}
+          value={text}
+          placeholder="Enter some text here"
+      />
+      <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+        <Text style={styles.buttonText}>Submit</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +29,40 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+
+  title: {
+    fontSize: 24,
+    paddingTop: 70,
+    marginBottom: 40,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontFamily: 'TimesNewRomanPS-BoldMT',
+  },
+
+  subtitle: {
+    fontSize: 16,
+    color: '#555',
+  },
+
+  input: {
+    height: 40,
+    width: '80%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+  },
+
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+  },
+
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  }
 });
